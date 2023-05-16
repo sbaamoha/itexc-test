862e3f40390275e5b84cfe08d6f7e9e108b74ac5
sbaamoha <sbaamohe@gmail.com>
2023-05-16 13:40:16 +0100

    changed the router method

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 // import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
-import { routes } from "./routes";
-import HomePage from "./pages/home";
-
+// import { routes } from "./routes";
+// import HomePage from "./pages/home";
+import Dashboard from "./pages/dashboard";
+import ForgotPassword from "./pages/forgotPassword";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
 // import { useSelector, useDispatch } from "react-redux";
 // import { decrement, increment } from "./utils/redux/slices/counterReducer";
 function App() {
@@ -11,10 +14,14 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          {routes.map(({ path, component }, i) => (
+          {/* {routes.map(({ path, component }, i) => (
             <Route key={i} path={path} Component={component} />
-          ))}
-          <Route path="/*" Component={HomePage} />
+          ))} */}
+          <Route path="/" Component={Dashboard} />
+          <Route path="/dashboard" Component={Dashboard} />
+          <Route path="/login" Component={Login} />
+          <Route path="/signup" Component={Signup} />
+          <Route path="/forgot-password" Component={ForgotPassword} />
         </Routes>
       </BrowserRouter>
     </>
